5100584c7cc9e3d1e143c00b1fa2f60fe38def41
liza0058 <lizastrizkova@gmail.com>
2022-01-21 23:55:55 +0300

    Initial project version

---
+++ js/backgroundAdd.js
@@ -0,0 +1,5 @@
+let ReplaceBackground = function() {
+    document.body.style.backgroundImage = 'url(https://weblinks.ru/wp-content/uploads/2021/04/ca4122027f64a35a54a00bc311c9ab7a.jpg)';
+}
+
+document.addEventListener("DOMContentLoaded", ReplaceBackground);
